24b32a848937be5cf27cc1ea8a069db64eaef2b9
LAPTOP-IPA0BIH5\Craig <craigplmr434@gmail.com>
2020-06-24 12:17:31 +0100

    Raycasting combat component

---
+++ RPG Project/Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        public void Attack(CombatTarget target)
+        {
+            print("Hyah!");
+        }
+    }
+}
+
--- RPG Project/Assets/Scripts/Control/PlayerController.cs
+++ RPG Project/Assets/Scripts/Control/PlayerController.cs
@@ -1,11 +1,35 @@
 ﻿using UnityEngine;
 using RPG.Movement;
+using System;
+using RPG.Combat;
 
 namespace RPG.Control
 {
     public class PlayerController : MonoBehaviour
     {
         void Update()
+        {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue;
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+        }
+
+        private void InteractWithMovement()
         {
             if (Input.GetMouseButton(0))
             {
@@ -15,13 +39,17 @@ namespace RPG.Control
 
         private void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            bool hasHit = Physics.Raycast(ray, out hit);
+            Ray ray = GetMouseRay();
+            bool hasHit = Physics.Raycast(ray, out RaycastHit hit);
             if (hasHit)
             {
                 GetComponent<Mover>().MoveTo(hit.point);
             }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
