06c58d30cd78989e6bae66f030480b51541bcc14
LAPTOP-IPA0BIH5\Craig <craigplmr434@gmail.com>
2020-06-24 11:55:27 +0100

    Namespaces

--- RPG Project/Assets/Scripts/Movement/Mover.cs
+++ RPG Project/Assets/Scripts/Movement/Mover.cs
@@ -1,37 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
 
-public class Mover : MonoBehaviour
+namespace RPG.Movement
 {
-
-    void Update()
+    public class Mover : MonoBehaviour
     {
-        if (Input.GetMouseButton(0))
+
+        void Update()
         {
-            MoveToCursor();
-
+            UpdateAnimator();
         }
-        UpdateAnimator();
-    }
 
-    private void MoveToCursor()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-        if (hasHit)
+        public void MoveTo(Vector3 destination)
         {
-            GetComponent<NavMeshAgent>().SetDestination(hit.point);
+            GetComponent<NavMeshAgent>().SetDestination(destination);
         }
-    }
 
-    private void UpdateAnimator()
-    {
-        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
-        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
-        float speed = localVelocity.z;
-        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
     }
 }
