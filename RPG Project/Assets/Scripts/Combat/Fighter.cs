9e28e90a315f9643254a9db58bb116d18629c3c4
LAPTOP-IPA0BIH5\Craig <craigplmr434@gmail.com>
2020-06-25 11:07:36 +0100

    Attack Animation Implementation

--- RPG Project/Assets/Scripts/Combat/Fighter.cs
+++ RPG Project/Assets/Scripts/Combat/Fighter.cs
@@ -11,9 +11,13 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
         Transform target = null;
+        float timeSinceLastAttack = 0;
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
             if (target == null) return;
             if (!GetIsInRange())
             {
@@ -21,9 +25,20 @@ namespace RPG.Combat
             }
             else
             {
-                GetComponent<Mover>().Cancel();
+                if (timeSinceLastAttack > timeBetweenAttacks)
+                {
+                    GetComponent<Mover>().Cancel();
+                    AttackBehaviour();              
+                }           
             }
         }
+
+        private void AttackBehaviour()
+        {
+            GetComponent<Animator>().SetTrigger("attack");
+            timeSinceLastAttack = 0;
+        }
+
         public void Attack(CombatTarget combatTarget)
         {
             GetComponent<ActionScheduler>().StartAction(this);
@@ -39,6 +54,11 @@ namespace RPG.Combat
         private bool GetIsInRange()
         {
             return Vector3.Distance(transform.position, target.position) < weaponRange;
+        }
+        //Animation Event
+        void Hit()
+        {
+
         }
     }
 }
