73b3254266805e887da410c7bfd9e15c5271bd63
LAPTOP-IPA0BIH5\Craig <craigplmr434@gmail.com>
2020-06-30 14:06:47 +0100

    Portal spawn points and selecting destination via enum

--- RPG Project/Assets/Scripts/Scene Management/Portal.cs
+++ RPG Project/Assets/Scripts/Scene Management/Portal.cs
@@ -1,17 +1,59 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 namespace RPG.SceneManagement
 {
     public class Portal : MonoBehaviour
     {
+        enum DestinationIdentifier
+        {
+            A,B,C,D,E
+        }
+
         [SerializeField] int sceneToLoad = -1;
+        [SerializeField] GameObject spawnPoint;
+        [SerializeField] DestinationIdentifier destination;
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.tag == "Player")
             {
-                SceneManager.LoadScene(sceneToLoad);
+                StartCoroutine(Transition());
             }
+        }
+
+        private IEnumerator Transition()
+        {
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            Portal otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+
+            Destroy(gameObject);
+        }
+
+        private void UpdatePlayer(Portal otherPortal)
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.transform.position);
+            player.transform.rotation = otherPortal.spawnPoint.transform.rotation;
+        }
+
+        private Portal GetOtherPortal()
+        {
+            foreach (Portal portal in FindObjectsOfType<Portal>())
+            {
+                if (portal == this) continue;
+                if (portal.destination != destination) continue;
+                return portal;
+            }
+
+            return null;
         }
     }
 
