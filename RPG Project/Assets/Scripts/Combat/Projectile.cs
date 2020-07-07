aa4fe1f49e9bdf38f9b7ed9bb22c8aa8d17cbe6a
LAPTOP-IPA0BIH5\Craig <craigplmr434@gmail.com>
2020-07-07 12:02:07 +0100

    Arrow Damage

--- RPG Project/Assets/Scripts/Combat/Projectile.cs
+++ RPG Project/Assets/Scripts/Combat/Projectile.cs
@@ -10,6 +10,7 @@ namespace RPG.Combat
 
         [SerializeField] float speed = 1;
         Health target = null;
+        float damage = 0;
 
         // Update is called once per frame
         void Update()
@@ -19,9 +20,10 @@ namespace RPG.Combat
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
 
-        public void SetTarget (Health target)
+        public void SetTarget (Health target, float damage)
         {
             this.target = target;
+            this.damage = damage;
         }
 
         private Vector3 GetAimLocation()
@@ -29,6 +31,15 @@ namespace RPG.Combat
             CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
             if (!targetCapsule) { return target.transform.position; }
             return target.transform.position + (Vector3.up * targetCapsule.height / 2);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.GetComponent<Health>() == target)
+            {
+                target.TakeDamage(damage);
+                Destroy(gameObject);
+            }
         }
     }
 }
