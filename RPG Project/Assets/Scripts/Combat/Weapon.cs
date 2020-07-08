467861e6b72f8e18acb1babdd85d914c1bbd9920
LAPTOP-IPA0BIH5\Craig <craigplmr434@gmail.com>
2020-07-08 10:27:13 +0100

    Arrow Trails, Weapon Pickup Prefab Variants, Destroy old weapon on pickup

--- RPG Project/Assets/Scripts/Combat/Weapon.cs
+++ RPG Project/Assets/Scripts/Combat/Weapon.cs
@@ -1,4 +1,5 @@
 ﻿using RPG.Core;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -16,17 +17,34 @@ namespace RPG.Combat
         [SerializeField] private bool isRightHanded = true;
         [SerializeField] Projectile projectile;
 
+        const string weaponName = "Weapon";
+
         public void Spawn(Transform rightHand, Transform leftHand, Animator animator)
         {
+            DestroyOldWeapon(rightHand, leftHand);
+
             if (equippedPrefab != null)
             {
                 Transform handTransform = GetTransform(rightHand, leftHand);
-                Instantiate(equippedPrefab, handTransform);
+                GameObject weapon =  Instantiate(equippedPrefab, handTransform);
+                weapon.name = weaponName;
             }
             if (animatorOverride != null)
             {
                 animator.runtimeAnimatorController = animatorOverride;
             }
+        }
+
+        private void DestroyOldWeapon(Transform rightHand, Transform leftHand)
+        {
+            Transform oldWeapon = rightHand.Find(weaponName);
+            if (oldWeapon == null)
+            {
+                oldWeapon = leftHand.Find(weaponName);
+            }
+            if (oldWeapon == null) { return; }
+            oldWeapon.name = "DESTROYING";
+            Destroy(oldWeapon.gameObject);
         }
 
         private Transform GetTransform(Transform rightHand, Transform leftHand)
