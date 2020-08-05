71fed011ec107876a4f043549e08d6cb0d2b4ac5
LAPTOP-IPA0BIH5\Craig <craigplmr434@gmail.com>
2020-08-05 09:41:37 +0100

    CInematic Saved

--- RPG Project/Assets/Scripts/Cinematics/CinematicTrigger.cs
+++ RPG Project/Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -1,11 +1,13 @@
-﻿using UnityEngine;
+﻿using GameDevTV.Saving;
+using UnityEngine;
 using UnityEngine.Playables;
 
 namespace RPG.Cinematics
 {
-    public class CinematicTrigger : MonoBehaviour
+    public class CinematicTrigger : MonoBehaviour, ISaveable
     {
         bool alreadyTriggered = false;
+
         private void OnTriggerEnter(Collider other)
         {
             if (!alreadyTriggered && other.gameObject.tag == "Player")
@@ -13,6 +15,16 @@ namespace RPG.Cinematics
                 GetComponent<PlayableDirector>().Play();
                 alreadyTriggered = true;
             }
+        }
+
+        public object CaptureState()
+        {
+            return alreadyTriggered;
+        }
+
+        public void RestoreState(object state)
+        {
+            alreadyTriggered = (bool)state;
         }
     }
 
